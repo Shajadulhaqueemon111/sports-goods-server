cca1242413d4cc87a578262e66dc2a6ea1a75d4e
Md Emon <mdshajdulhaqueemon@gmail.com>
2024-11-12 00:41:17 +0600

    solve this error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { AbouteRoutes } from "./modules/about/aboute.router";
-import { ProductRoute } from "./modules/AllProducts/products/product.router";
+
 import { SingleProductRoute } from "./modules/singleProduct/single.product.router";
 import { orderRoutes } from "./modules/Order/Order.router";
 import { paymentRoutes } from "./modules/Payment/Payment.router";
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/aboute", AbouteRoutes);
-app.use("/api/v1/product", ProductRoute);
+
 app.use("/api/v1/single", SingleProductRoute);
 app.use("/api/v1/order", orderRoutes);
 app.use("/api/v1/payment", paymentRoutes);
